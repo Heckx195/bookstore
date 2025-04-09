75570178d876b277f0ddbbbaf95c3828a3e269c5
Lars Scheid <heckx196@gmail.com>
2025-04-09 13:11:39 +0500

    feat(auth): Implemented book-store user login

--- main.go
+++ main.go
@@ -3,29 +3,40 @@ package main
 import (
 	"restapi/config"
 	"restapi/handlers"
+	"restapi/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Initialize DB
 	config.DBInit()
 
 	r := gin.Default()
 
-	// Book Routes
-	r.POST("/books", handlers.CreateBook)
-	r.GET("/books", handlers.GetBooks)
-	r.GET("/books/:id", handlers.GetBooksById)
-	r.PUT("/books/:id", handlers.UpdateBook)
-	r.DELETE("/books/:id", handlers.DeleteBook)
+	// Public routes
+	r.POST("/register", handlers.Register)
+	r.POST("/login", handlers.Login)
 
-	// Author Routes
-	r.POST("/authors", handlers.CreateAuthor)
-	r.GET("/authors", handlers.GetAuthors)
+	// Protected routes
+	protected := r.Group("/")
+	protected.Use(middleware.AuthMiddleware())
+	{
+		// Book Routes
+		r.POST("/books", handlers.CreateBook)
+		r.GET("/books", handlers.GetBooks)
+		r.GET("/books/:id", handlers.GetBooksById)
+		r.PUT("/books/:id", handlers.UpdateBook)
+		r.DELETE("/books/:id", handlers.DeleteBook)
 
-	// Category Routes
-	r.POST("/categories", handlers.CreateCategory)
-	r.GET("/categories", handlers.GetCategories)
+		// Author Routes
+		r.POST("/authors", handlers.CreateAuthor)
+		r.GET("/authors", handlers.GetAuthors)
+
+		// Category Routes
+		r.POST("/categories", handlers.CreateCategory)
+		r.GET("/categories", handlers.GetCategories)
+	}
 
 	r.Run(":8080")
 }
